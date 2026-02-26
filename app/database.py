8542aa4f85555938beb48a51a33f9c2208ec9760
Madhuri <96173977+prachithakur23567@users.noreply.github.com>
2026-02-26 22:47:26 +0530

    Updated database config for PostgreSQL and fixed deployment

--- app/database.py
+++ app/database.py
@@ -1,10 +1,12 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./hrms.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
